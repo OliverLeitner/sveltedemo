ecbfd60faa015ea37aec5068196f6ee9c56534e8
Oliver Leitner <never@neverslair-blog.net>
2021-03-16 20:58:37 +0100

    some svelte data

--- controller/HelloWorld.svelte
+++ controller/HelloWorld.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
     export let name: string;
+
+  	/*export let mytodo = fetch("https://jsonplaceholder.typescript.com/todos/1")
+      .then(response => response.json())
+      .then(todo => {
+          mytodo = todo;
+    })*/
+
+    /*import { onMount } from "svelte";
+    let non = [];
+    let gettingQuote = false;
+
+    onMount(() => {
+        getRandomQuote();
+    });
+
+    async function getRandomQuote() {
+        gettingQuote = true;
+        const res = await fetch("https://api.quotable.io/random");
+        const json = await res.json();
+        non = [...non, json];
+        gettingQuote = false;
+    }*/
 </script>
 
 <main>
--- routes/HelloWorld.svelte
+++ routes/HelloWorld.svelte
@@ -1,10 +1,58 @@
 <script lang="ts">
+    import axios from "axios";
 	import HelloWorld from "../controller/HelloWorld.svelte"
 	export let name: string; // lets place var from app.svelte into controller helloworld...
+
+	import { onMount } from "svelte";
+    let non = [];
+    // let gettingQuote = false;
+
+    onMount(() => {
+        getRandomQuote();
+    });
+
+    async function getRandomQuote() {
+        // gettingQuote = true;
+        //const res = await fetch("https://api.quotable.io/random");
+        const res = await axios.get("https://api.ip2country.info/ip?5.6.7.8")
+        const json = await res.data;
+        // non = [...non, json];
+        non = json
+        // gettingQuote = false;
+    }
+
+  /*import { onMount } from "svelte";
+  let mytodo;
+
+  onMount(
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(response => response.json())
+      .then(todo => {
+          mytodo = todo;
+      })  
+      );
+      */
 </script>
 
 <HelloWorld name={name}>
-	<p>some slot content</p>
+	<!--div>
+        {#each non as {author, content}}
+            <p>{content} - {author}</p>
+        {/each}
+        <button on:click={getRandomQuote} disabled={gettingQuote}>Get Quote</button>
+        {#if gettingQuote}
+            <span>Getting Quote...</span>
+        {/if}
+    </div-->
+    <div>
+        {#if non["countryCode"]}
+          <ul>
+            <li>{non["countryName"]}</li>
+          </ul>
+        {:else}
+          <p>loading.....</p>
+        {/if}
+      </div>
 </HelloWorld>
 
 <style>
